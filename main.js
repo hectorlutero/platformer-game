66d4fd589de6bdac3cc008819b2a0186b7914da1
Hector Siman <hectorlutero@hotmail.com>
2023-04-07 00:39:45 -0300

    new(): project setup

---
+++ main.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = 1024
+canvas.height = 576
+c.fillStyle = "white"
+c.fillRect(10, 10, canvas.width, canvas.height)
