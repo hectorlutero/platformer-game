301bc95492896a46de7887070cf12f455f4bad78
Hector Siman <hectorlutero@hotmail.com>
2023-04-07 01:01:36 -0300

    update(): implementing gravity engine

--- main.js
+++ main.js
@@ -5,3 +5,50 @@ canvas.width = 1024
 canvas.height = 576
 c.fillStyle = "white"
 c.fillRect(10, 10, canvas.width, canvas.height)
+
+const gravity = 0.5
+
+class Player {
+    constructor(position) {
+        this.position = position
+        this.velocity = {
+            x: 0, y: 1
+        }
+        this.height = 100
+    }
+
+    draw() {
+        c.fillStyle = "red"
+        c.fillRect(this.position.x, this.position.y, 100, this.height)
+    }
+
+    update() {
+        player.draw()
+        player2.draw()
+        this.position.y += this.velocity.y
+        if (this.position.y + this.height + this.velocity.y < canvas.height)
+            this.velocity.y += gravity
+        else this.velocity.y = 0
+    }
+}
+
+const player = new Player({
+    x: 0,
+    y: 0
+})
+const player2 = new Player({
+    x: 300,
+    y: 100
+})
+
+let y = 100
+let y2 = 100
+function animate() {
+    window.requestAnimationFrame(animate)
+    c.fillStyle = "white"
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    player.update()
+    player2.update()
+}
+
+animate()
